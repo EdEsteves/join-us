ea171b238c15ff833b68a7524524d30a4cfea176
Eduardo <eduardoestentotecinfo@gmail.com>
2018-07-05 01:28:55 -0300

    Atualizado1

--- config-server/express.js
+++ config-server/express.js
@@ -8,9 +8,7 @@ module.exports = function(){
 
     app.use(express.urlencoded({extended: true}));
 
-    app.use(serveStatic(path.join(__dirname, '../dist')));
-
-    
+    app.use(express.static(path.join(__dirname, '../dist')));
 
     app.use(express.json());
 
