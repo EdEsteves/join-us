12c50237f4f85ed6abc23f29aba3b18320864043
Eduardo <eduardoestentotecinfo@gmail.com>
2018-07-07 01:35:08 -0300

    Produto Component

--- src/services/produto-service.js
+++ src/services/produto-service.js
@@ -1,6 +1,6 @@
 const produtoService = {
   created() {
-  this.$http.get('./v1/produto'/*'http://localhost:3000/v1/produto'*/).then((response) => {
+  this.$http.get(/*'./v1/produto'*/'http://localhost:3000/v1/produto').then((response) => {
       this.produto = response.data;
       console.log(this.produto);
     }), function (error) {
